c2503743f7eed0f9da222c655eea841d36a75cbb
ALYSON FARIA JACOMINE <alyson.jacomine@hotmail.com>
2025-02-15 02:45:42 -0300

    Sync client viewport with monster can see

--- src/monster.cpp
+++ src/monster.cpp
@@ -103,7 +103,7 @@ const std::string& Monster::getNameDescription() const
 
 bool Monster::canSee(const Position& pos) const
 {
-	return Creature::canSee(getPosition(), pos, 9, 9);
+	return Creature::canSee(getPosition(), pos, Map::maxClientViewportX + 1, Map::maxClientViewportX + 1);
 }
 
 bool Monster::canWalkOnFieldType(CombatType_t combatType) const
