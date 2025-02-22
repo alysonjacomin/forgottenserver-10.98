71a92f77c4fa9c5ea34c01fabde7d13568d5d44a
ALYSON FARIA JACOMINE <alyson.jacomine@hotmail.com>
2025-02-22 19:16:03 -0300

    Fix magic level update after wearing items

--- src/movement.cpp
+++ src/movement.cpp
@@ -808,6 +808,7 @@ ReturnValue MoveEvent::EquipItem(MoveEvent* moveEvent, Player* player, Item* ite
 
 	if (needUpdateStats) {
 		player->sendStats();
+		player->sendSkills();
 	}
 
 	return RETURNVALUE_NOERROR;
@@ -890,6 +891,7 @@ ReturnValue MoveEvent::DeEquipItem(MoveEvent*, Player* player, Item* item, slots
 
 	if (needUpdateStats) {
 		player->sendStats();
+		player->sendSkills();
 	}
 
 	return RETURNVALUE_NOERROR;
