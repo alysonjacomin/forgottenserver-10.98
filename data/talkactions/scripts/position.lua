52b2edfdc6f28b609aa4ee0b1e0e2e834d9258aa
ALYSON FARIA JACOMINE <alyson.jacomine@hotmail.com>
2025-03-01 04:45:38 -0300

    Fix issue with position calculation in move function

--- data/talkactions/scripts/position.lua
+++ data/talkactions/scripts/position.lua
@@ -3,7 +3,7 @@ function onSay(player, words, param)
 		local split = param:split(",")
 		player:teleportTo(Position(split[1], split[2], split[3]))
 	else
-		local position = player:getPosition()
+		local position = Position(self):getNextPosition(direction)
 		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "Your current position is: " .. position.x .. ", " .. position.y .. ", " .. position.z .. ".")
 	end
 	return false
