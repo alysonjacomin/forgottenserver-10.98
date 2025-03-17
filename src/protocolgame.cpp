fe48878423a1653fa3020fc2d2e78ec14f6cbc1b
ALYSON FARIA JACOMINE <alyson.jacomine@hotmail.com>
2025-03-17 13:14:29 -0300

    Remove unused extern

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -5,7 +5,6 @@
 
 #include "protocolgame.h"
 
-#include "actions.h"
 #include "ban.h"
 #include "condition.h"
 #include "configmanager.h"
@@ -22,7 +21,6 @@
 #include "storeinbox.h"
 
 extern ConfigManager g_config;
-extern Actions actions;
 extern CreatureEvents* g_creatureEvents;
 extern Chat* g_chat;
 
