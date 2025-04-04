60d667df1859c3ad6c7a5f7b22f3e013be3eab70
ALYSON FARIA JACOMINE <alyson.jacomine@hotmail.com>
2025-04-04 17:50:24 -0300

    Added json.lua for use extendedOpcodeJSON

--- data/lib/lib.lua
+++ data/lib/lib.lua
@@ -1,8 +1,9 @@
+-- Compatibility library for our old Lua API AND Compatibility with OPCODE JSON
+dofile('data/lib/compat/compat.lua')
+dofile('data/lib/compat/json.lua')
+
 -- Core API functions implemented in Lua
 dofile('data/lib/core/core.lua')
-
--- Compatibility library for our old Lua API
-dofile('data/lib/compat/compat.lua')
 
 -- Debugging helper function for Lua developers
 dofile('data/lib/debugging/dump.lua')
