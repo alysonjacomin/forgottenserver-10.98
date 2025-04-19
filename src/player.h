79149224a86fa42bc79468e2a92c7b25b6722a7f
ALYSON FARIA JACOMINE <alyson.jacomine@hotmail.com>
2025-04-18 23:17:05 -0300

    Fix classicAttackSpeed makes server lag

--- src/player.h
+++ src/player.h
@@ -1284,6 +1284,7 @@ class Player final : public Creature, public Cylinder
 		uint32_t magLevel = 0;
 		uint32_t actionTaskEvent = 0;
 		uint32_t walkTaskEvent = 0;
+		uint32_t classicAttackEvent = 0;
 		uint32_t MessageBufferTicks = 0;
 		uint32_t accountNumber = 0;
 		uint32_t guid = 0;
