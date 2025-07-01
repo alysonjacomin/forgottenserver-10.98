f8f83e8791af1c0c47c7e608dea8a85060bec7f4
ALS <alyson.jacomine@hotmail.com>
2025-07-01 19:30:15 -0300

    Avoid duplicate getMountByID calls in tameMount and untameMount

--- src/player.cpp
+++ src/player.cpp
@@ -4056,12 +4056,8 @@ bool Player::toggleMount(bool mount) {
 }
 
 bool Player::tameMount(uint16_t mountId) {
-	if (!g_game.mounts.getMountByID(mountId)) {
-		return false;
-	}
-
 	Mount* mount = g_game.mounts.getMountByID(mountId);
-	if (hasMount(mount)) {
+	if (!mount || hasMount(mount)) {
 		return false;
 	}
 
@@ -4071,12 +4067,8 @@ bool Player::tameMount(uint16_t mountId) {
 }
 
 bool Player::untameMount(uint16_t mountId) {
-	if (!g_game.mounts.getMountByID(mountId)) {
-		return false;
-	}
-
 	Mount* mount = g_game.mounts.getMountByID(mountId);
-	if (!hasMount(mount)) {
+	if (!mount || !hasMount(mount)) {
 		return false;
 	}
 
